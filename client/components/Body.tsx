c6dbdfe4e8330ddd64581b7f0027df50fbc219c5
nattapatee <nattapat.ek@bcircle.co.th>
2019-10-10 14:11:05 +0700

    hacktober 2019 -5

--- client/components/Body.tsx
+++ client/components/Body.tsx
@@ -179,6 +179,7 @@ export class Body extends React.Component<any, State> {
     // hacktober 2019 -2
     // hacktober 2019 -3
      // hacktober 2019 -4
+      // hacktober 2019 -5
     public initSearchByAll = (startDate: Date, endDate: Date, app: string, ip: string) => {
         this.LogDate = []
         this.LogNow = []
